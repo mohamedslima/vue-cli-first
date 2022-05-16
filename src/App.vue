86617861a384a0512e099182a46d54739aa43f46
mohamedslima <hammaslima@gmail.com>
2022-05-16 15:21:34 +0200

    added AppTitle

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AppTitle />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import AppTitle from "./components/AppTitle.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    AppTitle,
   },
 };
 </script>
